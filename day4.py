baea9aef96611256a1e6b011d3ef0359e86ca9a8
= <=>
2022-12-04 00:33:48 -0500

    Day 4 Part 1 Attempt 2 - Correct First attempt incorrectness was caused by numbers being parsed as strings, which were being compared alphabetically, rather than as numeric integers

--- day4.py
+++ day4.py
@@ -6,7 +6,7 @@ with open("day4_input") as f:
         
         #process input into 2D list of numbers
         assignments = line.strip().split(',')
-        assignments = [assign_range.split('-') for assign_range in assignments]
+        assignments = [ [int(string) for string in assign_range.split('-')] for assign_range in assignments]
 
         print(assignments)
 
@@ -15,11 +15,13 @@ with open("day4_input") as f:
         # zeroth range fully contains first range
         if assignments[0][0] <= assignments[1][0] and assignments[0][1] >= assignments[1][1]:
             counter_containing += 1
-            print("enclosing")
+            print("zeroth contains first")
 
         # first range fully contains zeroth range
         elif assignments[0][0] >= assignments[1][0] and assignments[0][1] <= assignments[1][1]:
             counter_containing += 1
-            print("enclosing")
+            print("first contains zeroth")
 
     print(counter_containing)
+
+#first attempt - First attempt incorrectness was caused by numbers being parsed as strings, which were being compared alphabetically, rather than as numeric integers   
