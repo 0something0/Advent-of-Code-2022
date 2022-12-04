e5d2546bf6d2d6ec9a8aa41a931b9c09ed8d0dbe
= <=>
2022-12-04 00:52:01 -0500

    Day 4 Part 2 Attempt 2 - Correct First attempt - incorrect because I forgot to account for completely overlapping cases

--- day4_part2.py
+++ day4_part2.py
@@ -6,6 +6,13 @@
 # case 2
 # x < a < y < b
 
+# case 3
+# x < a < b < y
+
+# case 4
+# a < x < y < b
+
+
 # check if x is less than b and b is greater than y
 
 with open("day4_input") as f:
@@ -27,12 +34,28 @@ with open("day4_input") as f:
         y = assignments[1][1]
 
         #first range ends above zeroth range
+        #e.g. first range crosses zeroth range upper boundary
         if a <= x and x <= b and b <= y:
             counter_containing += 1
-            print('containing')
+            print('containing (zeroth < first)')
+
+        #zeroth range ends above first range
+        #e.g. zeroth range crosses first range upper boundary
         elif x <= a and a <= y and y <= b:
             counter_containing += 1
-            print('containing')
+            print('containing (first < zeroth)')
+
+        #completely containing (no overlap)
+        # zeroth range fully contains first range
+        elif a < x and y < b:
+            counter_containing += 1
+            print("zeroth contains first")
+
+        # first range fully contains zeroth range
+        elif x < a and b < y:
+            counter_containing += 1
+            print("first contains zeroth")
 
     print(counter_containing)
 
+#first attempt - incorrect because I forgot to account for completely overlapping cases
