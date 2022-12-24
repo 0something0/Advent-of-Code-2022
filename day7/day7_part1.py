cd8050a23195f910256306559c7039d93485761a
= <=>
2022-12-23 20:21:03 -0500

    Works with example data

--- day7/day7_part1.py
+++ day7/day7_part1.py
@@ -1,9 +1,11 @@
-
+def close_directory():
+    pass
 
 with open('day7_input.txt', 'r') as f:
 
-
     stack = [0]
+    stack_dirname = ['/']
+    list_of_dirs_under_100k = []
 
     for line in f:
 
@@ -21,13 +23,18 @@ with open('day7_input.txt', 'r') as f:
 
         # Entering directory
         elif line.strip()[:-1] == '$ cd ':
-            # stack.append(0)
-            # print(stack)
-            pass
+            stack.append(0)
+            stack_dirname.append(line.strip()[-1])
 
         # Exiting directory
         elif line.strip() == '$ cd ..':
-            pass
+
+            if stack[-1] <= 100000:
+                list_of_dirs_under_100k.append(stack_dirname[-1])
+
+            stack[-2] += stack[-1]
+            stack.pop()
+            stack_dirname.pop()
     
         # Check if line contains a directory size
         # Split line into two, attempt casat zeroth element to integer
@@ -37,3 +44,14 @@ with open('day7_input.txt', 'r') as f:
             stack[-1] = file_size + stack[-1]
 
         print(stack)
+        print(list_of_dirs_under_100k)
+
+    # clean up stack at the end
+    if stack[-1] <= 100000:
+        list_of_dirs_under_100k.append(stack_dirname[-1])
+
+    stack[-2] += stack[-1]
+    stack.pop()
+    stack_dirname.pop()
+
+
