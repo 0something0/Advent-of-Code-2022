b409a7fdc1e2dea5194c62f4174d439f2ac8fd31
= <=>
2022-12-23 20:04:50 -0500

    File size parsing

--- day7/day7_part1.py
+++ day7/day7_part1.py
@@ -3,9 +3,11 @@
 with open('day7_input.txt', 'r') as f:
 
 
+    stack = [0]
+
     for line in f:
 
-        # cases:
+        # Cases:
         # cd /
         # cd <directory>
         # cd ..
@@ -13,7 +15,25 @@ with open('day7_input.txt', 'r') as f:
         # <file size> [fileename]
         # dir [directory name]
 
-        if line.strip() == '$ cd /' or line.strip() == '$ ls' or line.strip()[:2] == 'dir':
+        # Filter for filler output
+        if line.strip() == '$ cd /' or line.strip() == '$ ls' or line.strip()[:3] == 'dir':
             pass
-        else:
-            print(line)
+
+        # Entering directory
+        elif line.strip()[:-1] == '$ cd ':
+            # stack.append(0)
+            # print(stack)
+            pass
+
+        # Exiting directory
+        elif line.strip() == '$ cd ..':
+            pass
+    
+        # Check if line contains a directory size
+        # Split line into two, attempt casat zeroth element to integer
+        # Relies on *every* other filter working, else casting will fail due to non-directory size text
+        elif int(line.split()[0]):
+            file_size = int(line.split()[0])
+            stack[-1] = file_size + stack[-1]
+
+        print(stack)
